2732395bc9f44c5308af68df86cd04f5094cf1b4
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-05-23 18:03:06 -0700

    Track migration

---
+++ GiveMeBooks/Migrations/20190523175708_FirstInitial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GiveMeBooks.Migrations
+{
+    public partial class FirstInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DbBooks",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ISBN = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Authors = table.Column<string>(nullable: true),
+                    Publisher = table.Column<string>(nullable: true),
+                    ImgUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DbBooks", x => x.BookId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DbBooks");
+        }
+    }
+}
