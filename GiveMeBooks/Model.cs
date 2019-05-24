b1f17f16ecc88915b03d95bd65fcef45cc815d40
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-05-23 17:45:18 -0700

    Created Database context for migration

---
+++ GiveMeBooks/Model.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiveMeBooks
+{
+    public class BooksContext : DbContext
+    {
+        public DbSet<Book> DbBooks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GiveMeBooksDB;Trusted_Connection=True;MultipleActiveResultSets=true;");
+        }
+
+    }
+
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        public string Publisher { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
